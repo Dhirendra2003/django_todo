51d01dfc9f73790d3a28a0e3a143716b9f3b14d0
dhirendra <shindedhirendra780@gmail.com>
2025-07-12 16:37:48 +0530

    todo complete crud

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect ,get_object_or_404
 from .models import Task
 
 # Create your views here.
@@ -7,3 +7,34 @@ def addTask(req):
     task=req.POST['task']
     Task.objects.create(task=task )
     return redirect('home')
+def mark_as_done(req, pk):
+    task=get_object_or_404(Task, pk=pk)
+    task.is_completed=True
+    task.save()
+
+    return redirect('home')
+
+def mark_as_undone(req, pk):
+    task=get_object_or_404(Task, pk=pk)
+    task.is_completed=False
+    task.save()
+
+    return redirect('home')
+
+def edit_task(req,pk):
+    task = get_object_or_404(Task, pk=pk)
+    if(req.method=='POST'):
+        task.task=req.POST['task_new']
+        task.save()
+        return  redirect('home')
+    else:
+        context={
+            'task':task
+        }
+        return render(req,'edit_task.html',context)
+
+def delete_task(req, pk):
+    task=get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home')
+
--- todo_main/views.py
+++ todo_main/views.py
@@ -4,10 +4,11 @@ from todo.models import Task
 
 def home(req):
   # return HttpResponse('<h1>Home page</h1>')
-  tasks=Task.objects.filter(is_completed=False).order_by('-created_at')
+  tasks=Task.objects.filter(is_completed=False).order_by('created_at')
+  completed_tasks=Task.objects.filter(is_completed=True).order_by('updated_at')
   # print(tasks)
   context={
     'tasks': tasks,
-
+    'completed_tasks':completed_tasks
   }
   return render(req ,'home.html',context)
